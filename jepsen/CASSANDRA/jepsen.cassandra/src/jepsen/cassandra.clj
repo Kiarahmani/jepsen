6330751275e3d57cdeb0518677f7bf47facce0c7
Kiarahmani <rahmank@purdue.edu>
2018-11-19 17:41:04 -0500

    ready to start configuting

--- jepsen/CASSANDRA/jepsen.cassandra/src/jepsen/cassandra.clj
+++ jepsen/CASSANDRA/jepsen.cassandra/src/jepsen/cassandra.clj
@@ -10,12 +10,49 @@
 
 
 
+;;====================================================================================
 (defn cached-install?
   [src]
   (try (c/exec :grep :-s :-F :-x (lit src) (lit ".download"))
        true
        (catch RuntimeException _ false)))
 
+;;====================================================================================
+(defn doInitJava!
+	"Installs Java on the given node"
+	[node version]
+	(info "Installing Java on node: " node)
+	(c/su
+   	(c/cd
+	(c/exec :echo
+     		"deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"
+     		:>"/etc/apt/sources.list.d/webupd8team-java.list")
+    	(c/exec :echo
+     		"deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"
+     		:>> "/etc/apt/sources.list.d/webupd8team-java.list")
+    		(try (c/exec :apt-key :adv :--keyserver "hkp://keyserver.ubuntu.com:80"
+              		:--recv-keys "EEA14886")
+        		(debian/update!)
+        	 	(catch RuntimeException e
+           			(info "Error updating caused by" e)))
+    		(c/exec :echo
+            		"debconf shared/accepted-oracle-license-v1-1 select true"
+            		| :debconf-set-selections)
+    		(debian/install [:oracle-java8-installer]))))
+
+(defn initJava!
+	"makes sure java exists and if not calls the installer"	
+	[node version]
+	(info "making sure Java exists")
+	(c/exec :echo
+		"java -version")
+	;(doInitJava! node version)
+; TODO: must automatically check if it exists
+)
+
+
+
+;;====================================================================================
 (defn install!
 	"Installs Cassandra on the given node"
 	[node version]
@@ -37,26 +74,18 @@
             (c/exec :tar :xzvf "cassandra.tar.gz" :-C "~")
             (c/exec :rm :-r :-f (lit "~/cassandra"))
             (c/exec :mv (lit "~/apache* ~/cassandra"))
-            (c/exec :echo url :> (lit ".download")))))
-(c/exec
-     :echo
-     "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"
-     :>"/etc/apt/sources.list.d/webupd8team-java.list")
-    (c/exec
-     :echo
-     "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main"
-     :>> "/etc/apt/sources.list.d/webupd8team-java.list")
-    (try (c/exec :apt-key :adv :--keyserver "hkp://keyserver.ubuntu.com:80"
-                :--recv-keys "EEA14886")
-         (debian/update!)
-         (catch RuntimeException e
-           (info "Error updating caused by" e)))
-    (c/exec :echo
-            "debconf shared/accepted-oracle-license-v1-1 select true"
-            | :debconf-set-selections)
-    (debian/install [:oracle-java8-installer]))))
+            (c/exec :echo url :> (lit ".download"))))))))
 
 
+
+;;====================================================================================
+(defn configure!
+  "Uploads configuration files to the given node."
+  [node test]
+  (info node "configuring Cassandra..."))
+
+
+;;====================================================================================
 (defn db
   "Cassandra for a particular version."
   [version]
@@ -64,7 +93,9 @@
     (setup! [_ test node]
       (info node "installing cassandra" version)
       (doto node      
-	(install! version)))
+	(install! version)
+	(initJava! version)
+	(configure! test)))
 
     (teardown! [_ test node]
       (info node "tearing down cassandra"))))
@@ -72,6 +103,7 @@
 
 
 
+;;====================================================================================
 (defn cassandra-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
@@ -82,6 +114,7 @@
           :os   debian/os
           :db   (db "3.11.3")}))
 
+;;====================================================================================
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
