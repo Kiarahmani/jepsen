ac5fac4743583e7c90b55293b911611c7bdd01b0
Ubuntu <ubuntu@ip-172-31-41-66.us-east-2.compute.internal>
2018-12-23 20:22:36 +0000

    transacitons created -> ready to implement their bodies

--- jepsen/CASSANDRA/jepsen.cassandra/src/jepsen/cassandra-operations.clj
+++ jepsen/CASSANDRA/jepsen.cassandra/src/jepsen/cassandra-operations.clj
@@ -4,10 +4,25 @@
 (def closeConnection (fn [conn]    (SeatsClient/closeConnection conn)))
 (def openConnection  (fn [ip]      (SeatsClient/getConnection ip)))
 
-(def operationMap [{:n 1, :f :DELETE_RESERVATION_TXN, 
+(def operationMap [{:n 1, :f :DR-TXN, 
                           :javaFunc (fn [conn args] (SeatsClient/deleteReservation conn (nth args 0)(nth args 1)(nth args 2)(nth args 3)(nth args 4))), 
-                          :freq 1}
-                   
+                          :freq 10/100},
+                   {:n 2, :f :FF-TXN, 
+                          :javaFunc (fn [conn args] (SeatsClient/findFlights conn (nth args 0)(nth args 1)(nth args 2)(nth args 3)(nth args 4))), 
+                          :freq 10/100},
+                   {:n 3, :f :FOS-TXN, 
+                          :javaFunc (fn [conn args] (SeatsClient/findOpenSeats conn (nth args 0))), 
+                          :freq 35/100},
+                   {:n 4, :f :NR-TXN, 
+                          :javaFunc (fn [conn args] (SeatsClient/newReservation conn (nth args 0)(nth args 1)(nth args 2)(nth args 3)(nth args 4)(nth args 5))), 
+                          :freq 20/100},
+                   {:n 5, :f :UC-TXN, 
+                          :javaFunc (fn [conn args] (SeatsClient/updateCustomer conn (nth args 0)(nth args 1)(nth args 2)(nth args 3)(nth args 4)(nth args 5)(nth args 6))), 
+                          :freq 10/100},
+                   {:n 6, :f :UR-TXN, 
+                          :javaFunc (fn [conn args] (SeatsClient/updateReservation conn (nth args 0)(nth args 1)(nth args 2)(nth args 3)(nth args 4)(nth args 5))), 
+                          :freq 15/100}
+
                    ])
 
 
@@ -48,6 +63,17 @@
             ff   (gen_ff)
             f    (gen_flight)]
       [(:f_id f),(:c_id cust), (:c_id_str cust), (:ff_c_id_str ff), (:ff_al_id ff)])
+    ;FindFlights
+    2  [1,1,1,1,1]
+    ;FindOpenSeats
+    3  [1]
+    ;NewReservation
+    4  [1,1,1,1,1,(make-array Integer/TYPE 3)]
+    ;UpdateCustomer
+    5  [1,1,"1",1,1,1,1]
+    ;UpdateReservation
+    6  [1,1,1,1,1,1]
+    (info "ERROR!! ---> UNKNOWN txnNo")
     ))
 
 ;====================================================================================================
